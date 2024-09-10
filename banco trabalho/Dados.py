04adec6a7f597103f54f5f2fdfbd2419f1567033
Eto12 <reverso12yt@gmail.com>
2024-09-10 15:48:58 -0300

    a

--- banco trabalho/Dados.py
+++ banco trabalho/Dados.py
@@ -3,20 +3,12 @@ def linha(bar):
     return print("-" * bar)
     
 def Ver_CPF(cpf):
-    # Remove caracteres não numéricos
-    cpf = ''.join(filter(str.isdigit, cpf))
-
-    # Verifica se o CPF tem 11 dígitos
-    if len(cpf) != 11:
+    if len(cpf) != 11 or not cpf.isdigit():
         return False
-
-    # Calcula o primeiro dígito verificador
-    soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
-    digito1 = (soma * 10 % 11) % 10
-
-    # Calcula o segundo dígito verificador
-    soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
-    digito2 = (soma * 10 % 11) % 10
-
-    # Verifica se os dígitos verificadores são iguais aos do CPF
-    return digito1 == int(cpf[9]) and digito2 == int(cpf[10])
+    else:
+        cpf = [int(digit) for digit in cpf]
+        soma1 = sum(cpf[i] * (10 - i) for i in range(9))
+        digito1 = (soma1 * 10 % 11) % 10
+        soma2 = sum(cpf[i] * (11 - i) for i in range(10))
+        digito2 = (soma2 * 10 % 11) % 10
+        return digito1 == cpf[9] and digito2 == cpf[10]
